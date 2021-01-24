eb46753216c94944d15bd41c367c777297840c8b
Kevin <kai821104@gmail.com>
2021-01-24 20:29:19 +0800

    move fetch cors data logic from index to controller

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { fetchData } from "./server/controller.js";
+import { fetchCorsData } from "./server/controller.js";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import cors from "cors";
@@ -8,12 +8,8 @@ const port = 4000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
 app.use(cors());
 app.use(express.static(__dirname + "/app/build"));
-app.get("/v1/cors*", async (req, res) => {
-  const corsTarget = req.url.replace("/v1/cors", "");
-  let data = await fetchData(corsTarget);
+app.get("/v1/cors*", fetchCorsData);
 
-  res.status(200).json(data);
-});
 app.listen(port, () => {
   console.log("running on port", port);
 });
--- server/controller.js
+++ server/controller.js
@@ -4,3 +4,10 @@ export async function fetchData(src) {
   let response = await axios.get(src);
   return response.data;
 }
+
+export async function fetchCorsData(req, res) {
+  const corsTarget = req.url.replace("/v1/cors", "");
+  let data = await fetchData(corsTarget);
+
+  res.status(200).json(data);
+}
